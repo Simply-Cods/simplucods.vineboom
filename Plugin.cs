1cc0fdc806b2152a3f1c9bb074257fdc9707a6d8
Simply-Cods <79521176+Simply-Cods@users.noreply.github.com>
2023-03-17 21:14:38 +0100

    much work very proud

---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace simplycods.vineboom
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "simplycods.vineboom";
+        public const string PLUGIN_NAME = "simplycods.vineboom";
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        public static ManualLogSource _logger;
+
+        private AssetBundle bundle;
+        public static AudioClip theboom;
+
+        private void Awake()
+        {
+            _logger = Logger;
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("simplycods.vineboom.Resources.vineboom"))
+            {
+                bundle = AssetBundle.LoadFromStream(stream);
+            }
+
+            theboom = bundle.LoadAsset<AudioClip>("vine boom sound effect");
+
+            new Harmony(PLUGIN_GUID).PatchAll();
+
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPatch(typeof(ItemIdentifier), "PickUp")]
+        public static class SkullPunch
+        {
+            private static ItemType[] _skulls = new[] { ItemType.SkullBlue, ItemType.SkullRed, ItemType.SkullGreen };
+
+            public static void Postfix(ItemIdentifier __instance)
+            {
+
+                if (_skulls.Contains(__instance.itemType))
+                {
+                    var skull = __instance.GetComponent<Skull>();
+                    var audioSource = Traverse.Create(skull).Field("aud").GetValue<AudioSource>();
+                    audioSource.clip = theboom;
+                    audioSource.Play();
+                }
+            }
+        }
+    }
+}
